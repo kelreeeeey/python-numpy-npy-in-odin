9ef282ac57f9533d7477329bfdd63bcb340193b7
kelreeeeey <kelreytaufiq08@mail.ugm.ac.id>
2025-09-19 03:27:05 +0700

    [add-feat] Add taskfile and some bashcript for quick test

--- scripts/test_all_floats.sh
+++ scripts/test_all_floats.sh
@@ -2,30 +2,30 @@
 
 test_2d_arrays() {
   # the complex numbers are not very intuitive for me, yet
-  ./builds/main.exe ./test_data/floats/cdouble_5x5.npy
-  ./builds/main.exe ./test_data/floats/clongdouble_5x5.npy
-  ./builds/main.exe ./test_data/floats/csingle_5x5.npy
-  ./builds/main.exe ./test_data/floats/double_5x5.npy
-  ./builds/main.exe ./test_data/floats/float16_5x5.npy
-  ./builds/main.exe ./test_data/floats/float32_5x5.npy
-  ./builds/main.exe ./test_data/floats/float64_5x5.npy
-  ./builds/main.exe ./test_data/floats/half_5x5.npy
-  ./builds/main.exe ./test_data/floats/longdouble_5x5.npy
-  ./builds/main.exe ./test_data/floats/single_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/cdouble_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/clongdouble_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/csingle_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/double_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/float16_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/float32_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/float64_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/half_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/longdouble_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/single_5x5.npy
 }
 
 test_1d_arrays() {
   # the complex numbers are not very intuitive for me, yet
-  ./builds/main.exe ./test_data/floats/cdouble_5.npy
-  ./builds/main.exe ./test_data/floats/clongdouble_5.npy
-  ./builds/main.exe ./test_data/floats/csingle_5.npy
-  ./builds/main.exe ./test_data/floats/double_5.npy
-  ./builds/main.exe ./test_data/floats/float16_5.npy
-  ./builds/main.exe ./test_data/floats/float32_5.npy
-  ./builds/main.exe ./test_data/floats/float64_5.npy
-  ./builds/main.exe ./test_data/floats/half_5.npy
-  ./builds/main.exe ./test_data/floats/longdouble_5.npy
-  ./builds/main.exe ./test_data/floats/single_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/cdouble_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/clongdouble_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/csingle_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/double_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/float16_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/float32_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/float64_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/half_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/longdouble_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/floats/single_5.npy
 }
 
 test_2d_arrays
--- scripts/test_all_ints.sh
+++ scripts/test_all_ints.sh
@@ -1,39 +1,39 @@
 #! usr/bin/ bash
 
 test_2d_arrays() {
-  ./builds/main.exe ./test_data/ints/b_5x5x5.npy
-  ./builds/main.exe ./test_data/ints/byte_5x5.npy
-  ./builds/main.exe ./test_data/ints/int8_5x5.npy
-  ./builds/main.exe ./test_data/ints/intc_5x5.npy
-  ./builds/main.exe ./test_data/ints/int__5x5.npy
-  ./builds/main.exe ./test_data/ints/int16_5x5.npy
-  ./builds/main.exe ./test_data/ints/int32_5x5.npy
-  ./builds/main.exe ./test_data/ints/int64_5x5.npy
-  ./builds/main.exe ./test_data/ints/longlong_5x5.npy
-  ./builds/main.exe ./test_data/ints/short_5x5.npy
-  ./builds/main.exe ./test_data/ints/ubyte_5x5.npy
-  ./builds/main.exe ./test_data/ints/uint8_5x5.npy
-  ./builds/main.exe ./test_data/ints/uintc_5x5.npy
-  ./builds/main.exe ./test_data/ints/ulonglong_5x5.npy
-  ./builds/main.exe ./test_data/ints/ushort_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/b_5x5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/byte_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int8_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/intc_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int__5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int16_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int32_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int64_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/longlong_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/short_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/ubyte_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/uint8_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/uintc_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/ulonglong_5x5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/ushort_5x5.npy
 }
 
 test_1d_arrays() {
-  ./builds/main.exe ./test_data/ints/int8_5.npy
-  ./builds/main.exe ./test_data/ints/intc_5.npy
-  ./builds/main.exe ./test_data/ints/int__5.npy
-  ./builds/main.exe ./test_data/ints/int16_5.npy
-  ./builds/main.exe ./test_data/ints/int32_5.npy
-  ./builds/main.exe ./test_data/ints/int64_5.npy
-  ./builds/main.exe ./test_data/ints/longlong_5.npy
-  ./builds/main.exe ./test_data/ints/short_5.npy
-  ./builds/main.exe ./test_data/ints/ubyte_5.npy
-  ./builds/main.exe ./test_data/ints/uintc_5.npy
-  ./builds/main.exe ./test_data/ints/ulonglong_5.npy
-  ./builds/main.exe ./test_data/ints/ushort_5.npy
-  ./builds/main.exe ./test_data/ints/b_5.npy
-  ./builds/main.exe ./test_data/ints/byte_5.npy
-  ./builds/main.exe ./test_data/ints/uint8_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int8_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/intc_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int__5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int16_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int32_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/int64_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/longlong_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/short_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/ubyte_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/uintc_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/ulonglong_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/ushort_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/b_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/byte_5.npy
+  ./builds/python-numpy-npy-in-odin.exe ./test_data/ints/uint8_5.npy
 }
 
 test_2d_arrays
